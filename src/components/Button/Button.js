552c6ecf3547842afc74512c389640765ffe7301
Mateusz Kaszubski <45321289+kaszubski@users.noreply.github.com>
2020-06-02 12:00:02 +0200

    Add proper btn components

---
+++ src/components/Button/Button.js
@@ -0,0 +1,27 @@
+import styled, { css } from 'styled-components';
+
+const Button = styled.button`
+  background-color: #FFD82B;
+  width: ${({ width }) => width || '220px'};
+  height: 47px;
+  border: none;
+  border-radius: 50px;
+  font-family: "Montserrat";
+  font-weight: 500;
+  font-size: 16px;
+  text-transform: uppercase;
+  :focus {
+    outline: 0;
+  }
+
+  ${({ secondary }) => (
+    secondary && css`
+      background-color: #E6E6E6;
+      width: 105px;
+      height: 30px;
+      font-size: 10px;
+    `
+  )}
+`;
+
+export default Button;
