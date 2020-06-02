2a8a05b5ce06d7bcf2551c8471befc9e73d00deb
Mateusz Kaszubski <45321289+kaszubski@users.noreply.github.com>
2020-06-02 13:33:19 +0200

    Add simple knobs to storybook

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,7 +1,7 @@
 import styled, { css } from 'styled-components';
 
 const Button = styled.button`
-  background-color: #FFD82B;
+  background-color: ${({ color }) => color || 'hsl(49, 100%, 58%)'};
   width: ${({ width }) => width || '220px'};
   height: 47px;
   border: none;
@@ -16,7 +16,7 @@ const Button = styled.button`
 
   ${({ secondary }) => (
     secondary && css`
-      background-color: #E6E6E6;
+      background-color: hsl(0, 0%, 90%);
       width: 105px;
       height: 30px;
       font-size: 10px;
--- src/components/Button/Button.stories.js
+++ src/components/Button/Button.stories.js
@@ -1,16 +1,30 @@
 import React from 'react';
+import { withKnobs, select } from '@storybook/addon-knobs';
 import Button from './Button';
 
 export default {
-  component: Button,
   title: 'Button',
+  decorators: [withKnobs],
 };
 
-export const Primary = () => (
-  <Button>
-    CLICK
-  </Button>
-);
+export const Primary = () => {
+  const label = 'Colors';
+  const options = {
+    Primary: 'hsl(48, 100%, 58%)',
+    Secondary: 'hsl(196, 83%, 75%)',
+    Tertiary: 'hsl(106, 47%, 64%)',
+  };
+  const defaultValue = 'hsl(49, 100%, 58%)';
+  const groupID = 'GROUP-ID1';
+
+  const value = select(label, options, defaultValue, groupID);
+
+  return (
+    <Button color={value}>
+      CLICK
+    </Button>
+  );
+};
 
 export const Secondary = () => (
   <Button secondary>
