c6968694b33a9cf53c97cd6f312ce3b462dfb401
Mateusz Kaszubski <45321289+kaszubski@users.noreply.github.com>
2020-06-03 10:44:54 +0200

    Progress before input

--- src/components/atoms/Button/Button.js
+++ src/components/atoms/Button/Button.js
@@ -1,13 +1,14 @@
 import styled, { css } from 'styled-components';
 
 const Button = styled.button`
+  padding: 0;
   background-color: ${({ theme }) => theme.primary};
   width: 220px;
   height: 47px;
   border: none;
   border-radius: 50px;
   font-family: "Montserrat";
-  font-weight: 500;
+  font-weight: 600;
   font-size: 16px;
   text-transform: uppercase;
   :focus {
